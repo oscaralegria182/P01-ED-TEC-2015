3f229ee475c50c615cd4bf01d9c11277dd4811ab
oscaralegria182 <oscaralegria182@gmail.com>
2015-03-31 11:33:38 -0600

    Test REGEX

--- testRegex.java
+++ testRegex.java
@@ -2,14 +2,59 @@ import java.util.regex.*;
 
 public class testRegex
 {
-	public static void main(String args[])
+// 	public static void main(String args[]) throws Exception
+// 	{
+      public static void main(String[] args) throws Exception
+      {
+	// compilamos el patron
+ 	Pattern patron = Pattern.compile("[\\<][a-z][\\>]");
+ 	// creamos el Matcher a partir del patron, la cadena como parametro
+ 	Matcher encaja = patron.matcher("<div a=6> \n hola\n <div>");
+ 	// invocamos el metodo replaceAll
+ 	if (encaja.find())
 	{
-		// compilamos el patron
-		Pattern patron = Pattern.compile("<>");
-		// creamos el Matcher a partir del patron, la cadena como parametro
-		Matcher encaja = patron.matcher("<div a=6> hola <div>");
-		// invocamos el metodo replaceAll
-		String resultado = encaja.replaceAll(" ");
-		System.out.println(resultado);
+		String resultado2 = encaja.replaceAll(":)");
+ 		System.out.println("Resultado:\t"+resultado2);
 	}
+	  String input = "www.?regular.com";
+	  // comprueba que no empieze por punto o @
+	  Pattern p = Pattern.compile("^.|^@");
+	  Matcher m = p.matcher(input);
+	  if (m.find())
+	    System.err.println("Las direcciones email no empiezan por punto o @");
+	    
+	  // comprueba que no empieze por www.
+	  p = Pattern.compile("^www.");
+	  m = p.matcher(input);
+	  if (m.find())
+	    System.out.println("Los emails no empiezan por www");
+
+	  // comprueba que contenga @
+	  p = Pattern.compile("@");
+	  m = p.matcher(input);
+	  if (!m.find())
+	    System.out.println("La cadena no tiene arroba");
+	    
+	  // comprueba que no contenga caracteres prohibidos	
+	  p = Pattern.compile("[^A-Za-z0-9.@_-~#]+");
+	  m = p.matcher(input);
+	  StringBuffer sb = new StringBuffer();
+	  boolean resultado = m.find();
+	  boolean caracteresIlegales = false;
+
+	  while(resultado) {
+	    caracteresIlegales = true;
+	    m.appendReplacement(sb, "");
+	    resultado = m.find();
+	  }
+
+	  // Añade el ultimo segmento de la entrada a la cadena
+	  m.appendTail(sb);
+
+	  input = sb.toString();
+
+	  if (caracteresIlegales) {
+	    System.out.println("La cadena contiene caracteres ilegales");
+	  }
+      }
 }
