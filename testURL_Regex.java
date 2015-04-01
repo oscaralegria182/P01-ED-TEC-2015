f525285122da9c292b22e9237adc3fc905d8e2eb
oscaralegria182 <oscaralegria182@gmail.com>
2015-03-31 19:42:48 -0600

    Prueba 1 descargando web page

--- testURL_Regex.java
+++ testURL_Regex.java
@@ -3,45 +3,65 @@ import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.regex.*;
- 
+import java.io.Console;
 
-public class PruebaURL
+
+public class testURL_Regex
 {
        
     public static void main(String[] args) 
     {
+        Console console = System.console();
         try {
             // Url con la foto
             URL url = new URL("http://www.ucr.ac.cr");
- 
-            // establecemos conexion
-            URLConnection urlCon = url.openConnection();
- 
-            // Sacamos por pantalla el tipo de fichero
-            System.out.println(urlCon.getContentType());
- 
-            // Se obtiene el inputStream de la foto web y se abre el fichero
-            // local.
-            InputStream is = url.openStream();//.getInputStream();
+	    URLConnection urlCon = url.openConnection();
+	    System.out.println(urlCon.getContentType());
+	    
+	    InputStream is = url.openStream();//.getInputStream();
             FileOutputStream fos = new FileOutputStream("web.txt");
  
-            // Lectura de la foto de la web y escritura en fichero local
-            byte[] array = new byte[10]; // buffer temporal de lectura.
+            byte[] array = new byte[10000]; // buffer temporal de lectura.
             int leido = is.read(array);
+            //String temp = new String(array);
+            //System.out.println(temp);
            // System.out.println("Leido Out:\t"+leido);
-            while (leido > 0) {
-                fos.write(array, 0, leido);
-//                 System.out.println("Leido In:\t"+leido);
-                //for(int i=0;i<leido;i++)
-// 	          //      System.out.println((char)array[i]);
-                leido = is.read(array);
-            }
- 
-            // cierre de conexion y fichero.
-            is.close();
-            fos.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            while (leido > 0) 
+            {
+                
+                String temp = new String(array);
+                //System.out.println(temp);
+                String cadena="[<.*>].*[<.*>]";//(<.*>)[^.*]</title>
+		Pattern patron = Pattern.compile(cadena);
+		String resultado2="";
+		Matcher matcher = patron.matcher(temp);
+		boolean found = false;
+		while (matcher.find()) 
+		{
+		 
+		 //console.format("I found the text" +" \"%s\" starting at " + "index %d and ending at index %d.%n", matcher.group(), matcher.start(),matcher.end());
+		 resultado2 = matcher.replaceAll("");
+		  System.out.println("Resultado:\t"+resultado2);
+		  found = true;
+		 }
+		 
+		 //char[] array2 = resultado.toCharArray();
+		 
+		 fos.write(resultado2.getBytes(), 0,resultado2.getBytes().length);
+		 
+		 leido = is.read(array);
+	  if(!found)
+	  {
+		console.format("No match found.%n");
+	  }
+		  
+	      }
+  
+	      // cierre de conexion y fichero.
+	      is.close();
+	      fos.close();
+	  } catch (Exception e) {
+	      e.printStackTrace();
+	  }
     }
 }
